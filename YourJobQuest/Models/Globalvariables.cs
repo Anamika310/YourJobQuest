e05cfa09fee388fef2d788f3e93989bf6e133f4e
Anamika310 <anamika@mtxbd.com>
2012-10-29 17:23:02 +0600

    master

---
+++ YourJobQuest/Models/Globalvariables.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Data.SqlClient;
+
+namespace YourJobQuest.Models
+{
+    public class Globalvariables
+    {
+        public static int UserId { get; set; }
+        public static String UserName { get; set; }
+        public static YourJobQuestEntities1 db { get; set; }
+        public static SqlCommand conn { get; set; }
+        public static String ProfileName { get; set; }
+        public static String email { get; set; }
+    }
+}
+
+
---
+++ YourJobQuest/Models/YourJobQuest.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'C:\Users\DevTwo\Documents\Visual Studio 2012\Projects\YourJobQuest\YourJobQuest\Models\YourJobQuest.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
